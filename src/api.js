3618ea4db98db7b6113eb560e230f9bee81d7703
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-01 19:23:50 +0000

    Add gitingore

--- src/api.js
+++ src/api.js
@@ -1,6 +1,4 @@
-// Substitua o CHAVE_DA_API com a chave gerada no site.
-
-export const API_KEY = '15717fe70bddd890087f7bc5c818f0f0';
+export const API_KEY = ''; //<--Your Api Key here
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
